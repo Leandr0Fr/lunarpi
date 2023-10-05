3ccbb45828ad0c74e4a13e1aef00d30e1ceafe94
leandroFr <leandro00fr@gmail.com>
2023-10-05 14:16:16 -0300

    FIX CORS

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, jsonify, request, make_response, render_template
 import os
 from model import prediction
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def hello_world():
